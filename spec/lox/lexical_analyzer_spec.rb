cbf68fc90b645622ae141cececa16ed671705038
Richard E. Dodson <richard.elias.dodson@gmail.com>
2017-12-03 21:35:27 -0500

    Add Lox::LexicalAnalyzer spec cases

--- spec/lox/lexical_analyzer_spec.rb
+++ spec/lox/lexical_analyzer_spec.rb
@@ -14,29 +14,88 @@ RSpec.describe Lox::LexicalAnalyzer do
       lexical_analyzer.each_token
     end
 
-    let :input_characters do
-      "print(\"Hello, world!\");\n".chars
-    end
-
-    let :tokens do
-      [
-        [:identifier, 'print'],
-        ['('],
-        [:string, 'Hello, world!'],
-        [')'],
-        [';']
-      ]
-    end
-
     before do
       allow(input).to receive(:each_char) do |&block|
         input_characters.each(&block)
       end
     end
 
-    it 'yields tokens' do
-      expect { |block| each_token.each(&block) }
-        .to yield_successive_args(*tokens)
+    context 'with a comment' do
+      let :input_characters do
+        '# this is a comment'.chars
+      end
+
+      let :tokens do
+        []
+      end
+
+      it 'yields tokens' do
+        expect { |block| each_token.each(&block) }
+          .to yield_successive_args(*tokens)
+      end
+    end
+
+    context 'with an expression' do
+      let :input_characters do
+        'index == 12 + 34'.chars
+      end
+
+      let :tokens do
+        [
+          [:identifier, 'index'],
+          ['=='],
+          [:integer, 12],
+          ['+'],
+          [:integer, 34]
+        ]
+      end
+
+      it 'yields tokens' do
+        expect { |block| each_token.each(&block) }
+          .to yield_successive_args(*tokens)
+      end
+    end
+
+    context 'with an expression' do
+      let :input_characters do
+        'count != 56 * index'.chars
+      end
+
+      let :tokens do
+        [
+          [:identifier, 'count'],
+          ['!='],
+          [:integer, 56],
+          ['*'],
+          [:identifier, 'index']
+        ]
+      end
+
+      it 'yields tokens' do
+        expect { |block| each_token.each(&block) }
+          .to yield_successive_args(*tokens)
+      end
+    end
+
+    context 'with a statement' do
+      let :input_characters do
+        "print(\"Hello, world!\");\n".chars
+      end
+
+      let :tokens do
+        [
+          [:identifier, 'print'],
+          ['('],
+          [:string, 'Hello, world!'],
+          [')'],
+          [';']
+        ]
+      end
+
+      it 'yields tokens' do
+        expect { |block| each_token.each(&block) }
+          .to yield_successive_args(*tokens)
+      end
     end
   end
 end
